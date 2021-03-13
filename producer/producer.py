e119adb5e069088758701d873503d6ec0f917b11
aliinejad <m.alinejad.h@gmail.com>
2021-03-13 18:54:52 +0330

    change localhost to kafka service name

--- producer/producer.py
+++ producer/producer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaProducer
 import time
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='kafka-service:9092')
 while True :
     producer.send('input', bytes(str(time.time()) , 'utf-8'))
     time.sleep(1)
