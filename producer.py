9d1b0864365517c64487a8165e7161f956fd7c8d
aliinejad <m.alinejad.h@gmail.com>
2021-03-13 11:04:02 +0330

    make seprate dir

--- producer.py
+++
@@ -1,9 +0,0 @@
- 
-from kafka import KafkaProducer
-import time
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-while True :
-    t = str(time.time())
-    producer.send('input', t )
-    time.sleep(1)
-
