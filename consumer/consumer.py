d1db69eff7be5a3992a99873f8a716183b29a756
aliinejad <m.alinejad.h@gmail.com>
2021-03-14 12:46:03 +0330

    add bytes() and add kafka service name

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,11 +3,11 @@ from kafka import KafkaProducer
 import time 
 
 
-consumer = KafkaConsumer (bootstrap_servers = 'kafka-service:9092')
+consumer = KafkaConsumer (bootstrap_servers = 'my-release-kafka:9092')
 consumer.subscribe(['input'])
-producer = KafkaProducer(bootstrap_servers='kafka-service:9092')
+producer = KafkaProducer(bootstrap_servers='my-release-kafka:9092')
 
 for msg in consumer:
      epoch_time = msg.value
      standard_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(epoch_time)))
-     producer.send('output', standard_time )
+     producer.send('output', bytes(standard_time , 'utf-8') )
