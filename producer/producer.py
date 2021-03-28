1c4c916b7747a872dc64db4c0a3a172fdf76deda
aliinejad <you@example.com>
2021-03-28 10:28:09 +0000

    add env variable to producer.py for finding kafka service

--- producer/producer.py
+++ producer/producer.py
@@ -1,6 +1,9 @@
 from kafka import KafkaProducer
 import time
-producer = KafkaProducer(bootstrap_servers='my-release-kafka:9092')
+import os
+key = "KAFKA_SERVICE_NAME"
+kafka = os.getenv(key)
+producer = KafkaProducer(bootstrap_servers = f'{kafka}:9092')
 while True :
     producer.send('input', bytes(str(time.time()) , 'utf-8'))
     time.sleep(1)
