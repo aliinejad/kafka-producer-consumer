61e364d312a4c86905ffc86e5dd161a789866095
aliinejad <m.alinejad.h@gmail.com>
2021-04-22 15:18:19 +0430

    add prometheus metric to code

--- producer/producer.py
+++ producer/producer.py
@@ -1,10 +1,16 @@
+from prometheus_client import start_http_server,Counter
 from kafka import KafkaProducer
 import time
 import os
+
+
 key = "KAFKA_SERVICE_NAME"
 kafka = os.getenv(key)
 producer = KafkaProducer(bootstrap_servers = f'{kafka}:9092')
+start_http_server(8000)
+c = Counter('my_failures', 'Description of counter')
 while True :
     producer.send('input', bytes(str(time.time()) , 'utf-8'))
+    c.inc()
     time.sleep(1)
 
