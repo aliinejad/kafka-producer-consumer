cfc275994a8581aaa56f335d1066c1c6d4697445
aliinejad <m.alinejad.h@gmail.com>
2021-04-22 16:14:40 +0430

    add prometheus metric to consumer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,3 +1,4 @@
+from prometheus_client import start_http_server,Counter
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 import time 
@@ -9,8 +10,11 @@ kafka = os.getenv(key)
 consumer = KafkaConsumer (bootstrap_servers = f'{kafka}:9092')
 consumer.subscribe(['input'])
 producer = KafkaProducer(bootstrap_servers = f'{kafka}:9092')
+start_http_server(8000)
+p = Counter('produced_massage ', 'Description of counter')
 
 for msg in consumer:
      epoch_time = msg.value
      standard_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(epoch_time)))
      producer.send('output', bytes(standard_time , 'utf-8') )
+     p.inc()
--- producer/producer.py
+++ producer/producer.py
@@ -8,7 +8,7 @@ key = "KAFKA_SERVICE_NAME"
 kafka = os.getenv(key)
 producer = KafkaProducer(bootstrap_servers = f'{kafka}:9092')
 start_http_server(8000)
-c = Counter('my_failures', 'Description of counter')
+c = Counter('produce_massage', 'Description of counter')
 while True :
     producer.send('input', bytes(str(time.time()) , 'utf-8'))
     c.inc()
